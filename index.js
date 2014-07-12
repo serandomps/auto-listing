4f6a9451a8e217fa58b8e1ca8ecc6f651784554d
ruchiraw <ruchira.wageesha@gmail.com>
2014-07-12 14:19:58 +0530

    fixing issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var list = function (el, options, paging, fn) {
     $.ajax({
         url: '/apis/v/vehicles',
         headers: {
-            'x-host': 'auto.serandives.com:4000'
+            'x-host': 'auto.serandives.com:4004'
         },
         dataType: 'json',
         success: function (data) {
