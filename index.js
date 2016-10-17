4f0a6684e0f5df5f87c3c45e586d36ab1af5c2bf
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-10-17 17:18:24 +0530

    updating

--- index.js
+++ index.js
@@ -5,28 +5,6 @@ var Vehicle = require('vehicle-service');
 
 var user;
 
-var query = function (options) {
-    if (!options) {
-        return '';
-    }
-    var data = {
-        query: {}
-    };
-    var name;
-    var value;
-    for (name in options) {
-        if (!options.hasOwnProperty(name)) {
-            continue;
-        }
-        if (name === '_') {
-            continue;
-        }
-        value = options[name];
-        data.query[name] = value instanceof Array ? {$in: value} : value;
-    }
-    return '?data=' + JSON.stringify(data);
-};
-
 var list = function (sandbox, options, fn) {
     Vehicle.find({query: options, images: '288x162'}, function (err, vehicles) {
         if (err) {
@@ -47,40 +25,6 @@ var list = function (sandbox, options, fn) {
             });
         });
     });
-    /*$.ajax({
-        url: utils.resolve('autos://apis/v/vehicles' + query(options)),
-        dataType: 'json',
-        success: function (data) {
-            dust.render('auto-listing', autils.cdn288x162(data), function (err, out) {
-                $('.auto-listing', sandbox).remove();
-                sandbox.off('click', '.auto-sort .btn');
-                sandbox.append(out);
-                sandbox.on('click', '.auto-sort .btn', function () {
-                    var sort = $(this).attr('name');
-                    var serand = require('serand');
-                    serand.emit('auto', 'sort', {sort: sort});
-                    list(options, {
-                        sort: sort
-                    });
-                });
-                sandbox.on('click', '.edit', function (e) {
-                    serand.redirect($(this).closest('.thumbnail').attr('href') + '/edit');
-                    return false;
-                });
-                if (!fn) {
-                    return;
-                }
-                fn(false, function () {
-                    $('.auto-listing', sandbox).remove();
-                });
-            });
-        },
-        error: function () {
-            fn(true, function () {
-
-            });
-        }
-    });*/
 };
 
 dust.loadSource(dust.compile(require('./template'), 'auto-listing'));
