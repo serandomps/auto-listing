a47ca123e54ae5e662111ef8e4612740f3a7161c
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-11-28 01:40:30 +0530

    updating

--- index.js
+++ index.js
@@ -1,46 +1,21 @@
 var dust = require('dust')();
 var serand = require('serand');
 var utils = require('utils');
-var Vehicle = require('vehicle-service');
 
-var user;
+dust.loadSource(dust.compile(require('./template'), 'autos-listing'));
 
-var list = function (sandbox, options, fn) {
-    Vehicle.find({query: options, images: '288x162'}, function (err, vehicles) {
-        if (err) {
-            fn(true, serand.none);
+module.exports = function (sandbox, fn, options) {
+    dust.render('autos-listing', options, function (err, out) {
+        sandbox.append(out);
+        sandbox.on('click', '.edit', function (e) {
+            serand.redirect($(this).closest('.thumbnail').attr('href') + '/edit');
+            return false;
+        });
+        if (!fn) {
             return;
         }
-        dust.render('auto-listing', vehicles, function (err, out) {
-            sandbox.append(out);
-            sandbox.on('click', '.edit', function (e) {
-                serand.redirect($(this).closest('.thumbnail').attr('href') + '/edit');
-                return false;
-            });
-            if (!fn) {
-                return;
-            }
-            fn(false, function () {
-                $('.auto-listing', sandbox).remove();
-            });
+        fn(false, function () {
+            $('.autos-listing', sandbox).remove();
         });
     });
 };
-
-dust.loadSource(dust.compile(require('./template'), 'auto-listing'));
-
-module.exports = function (sandbox, fn, options) {
-    list(sandbox, options, fn);
-};
-
-serand.on('user', 'ready', function (usr) {
-    user = usr;
-});
-
-serand.on('user', 'logged in', function (usr) {
-    user = usr;
-});
-
-serand.on('user', 'logged out', function (usr) {
-    user = null;
-});
